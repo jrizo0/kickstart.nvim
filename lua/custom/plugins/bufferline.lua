902cff87c67a5d8451c870199deac3112a29bac5
Julian Rizo <jr.rizo.o.jr@gmail.com>
2024-02-12 09:16:50 -0500

    updates

--- lua/custom/plugins/bufferline.lua
+++ lua/custom/plugins/bufferline.lua
@@ -7,6 +7,7 @@ return {
       { "<a-->",      function() require("mini.bufremove").delete(0, false) end, desc = "Delete Buffer" },
       { "<leader>bd", function() require("mini.bufremove").delete(0, false) end, desc = "Delete Buffer" },
       { "<leader>bD", function() require("mini.bufremove").delete(0, true) end,  desc = "Delete Buffer (Force)" },
+      { "<leader>ba", "<CMD>bufdo bd<CR>",                                       desc = "Delete All Buffers" },
     },
   },
 
